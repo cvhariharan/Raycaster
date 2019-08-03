d1bd8adb4790ad946294c38b8bb3d23b0454fa32
C.V.Hariharan <cvhariharan@protonmail.com>
2019-08-03 11:43:54 +0530

    Player added

--- engine.py
+++ engine.py
@@ -14,13 +14,15 @@ class Wall:
 
 class Ray:
     def __init__(self, surface, x1, y1, theta):
+        self.r = 1000
         self.x1 = x1
         self.y1 = y1
-        self.x2 = 100*math.cos(theta)
-        self.y2 = 100*math.sin(theta)
+        self.x2 = self.x1 + self.r * math.cos(theta)
+        self.y2 = self.y1 + self.r * math.sin(theta)
         self.surface = surface
     
     def draw(self):
+        # pygame.draw.circle(self.surface, (25, 100, 200), (self.x1, self.y1), 5)
         pygame.draw.line(self.surface, (255, 255, 255), (self.x1, self.y1), (self.x2, self.y2))
 
     def cast(self, wall):
@@ -40,10 +42,29 @@ class Ray:
             if (0 <= t and t <= 1) and u < 0:
                 px = round(x1 + t*(x2 - x1))
                 py = round(y1 + t*(y2 - y1))
-                pygame.draw.circle(self.surface, (255, 255, 255), (px, py), 3)
+                # pygame.draw.circle(self.surface, (255, 255, 255), (px, py), 3)
+                self.x2 = px
+                self.y2 = py
         except ZeroDivisionError:
             pass
        
+
+class Player:
+    def __init__(self, surface, x1, y1, walls):
+        self.x1 = x1
+        self.y1 = y1
+        self.surface = surface
+        self.walls = walls
+
+    def draw(self):
+        pygame.draw.circle(self.surface, (25, 100, 200), (self.x1, self.y1), 5)
+        for i in range(360):
+            if i % 2 == 0:
+                r = Ray(self.surface, self.x1, self.y1, math.radians(i))
+                for j in range(len(self.walls)):
+                    r.cast(self.walls[j])
+                r.draw()
+
 
 pygame.init()
 
@@ -54,10 +75,11 @@ clock = pygame.time.Clock()
 
 crashed = False
 
-w1 = Wall(disp, 50, 50, 300, 70)
+w1 = Wall(disp, 50, 50, 50, 200)
 w2 = Wall(disp, 100, 150, 400, 300)
-r = Ray(disp, 100, 100, math.radians(45))
-
+# r = Ray(disp, 100, 100, math.radians(-45))
+walls = [w1, w2]
+p = Player(disp, 100, 100, walls)
 while not crashed:
     disp.fill((0, 0, 0))
     for event in pygame.event.get():
@@ -70,10 +92,10 @@ while not crashed:
         # print(event)
     w1.draw()
     w2.draw()
-    r.draw()
-
-    r.cast(w1)
-    r.cast(w2)
+    # r.draw()
+    p.draw()
+    # r.cast(w1)
+    # r.cast(w2)
 
     pygame.display.update()
     clock.tick(60)
